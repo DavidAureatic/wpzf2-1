9666a8ae1ba150c746cb8b371b91b5f498243e05
unknown <davidcuellasgonzalez@gmail.com>
2014-03-17 20:07:07 +0100

    version 4

--- config/application.config.php
+++ config/application.config.php
@@ -15,6 +15,9 @@ return array(
     	'ZendDeveloperTools', 
     	'ZfcBase',
         'ZfcUser',
+    	'ZfcAdmin',
+    	'ZfcUserAdmin',
+    	'BjyAuthorize'
     ),
     'module_listener_options' => array(
         'module_paths' => array(
